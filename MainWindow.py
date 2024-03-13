de9ba95661d64acf1748b55acf0c4ae8ffe54e96
Stadnikov Rostislav <stadnikov2000@gmail.com>
2024-03-13 13:45:56 +0200

    Created save and load configuration

--- MainWindow.py
+++ MainWindow.py
@@ -13,16 +13,23 @@ class MainWindow:
     def createSliders(self):
         self.scale_w = Scale(self.root, from_ = 20, to = 1000, orient='horizontal', length= 200, command=self.changeSliderWidth)
         self.scale_h = Scale(self.root, from_ = 20, to = 400, orient='horizontal', length= 200, command=self.changeSliderHeight)
+
+        self.scale_x = Scale(self.root, from_ = 20, to = 1000, orient='horizontal', length= 200, command=self.changeSliderX)
+        self.scale_y = Scale(self.root, from_ = 20, to = 700, orient='horizontal', length= 200, command=self.changeSliderY)
+        self.scale_segmets = Scale(self.root, from_ = 10, to = 40, orient='horizontal', length= 200, command=self.changeSliderSegments)
+        self.scaleOpacity = Scale(self.root, from_ = 100, to = 20, orient='horizontal', length= 200, command=self.changeSliderOpacity)
+
         self.scale_w.pack()
         self.scale_w.set(200)
         self.scale_h.pack()
         self.scale_h.set(200)
-        Scale(self.root, from_ = 20, to = 1000, orient='horizontal', length= 200, command=self.changeSliderX).pack()
-        Scale(self.root, from_ = 20, to = 700, orient='horizontal', length= 200, command=self.changeSliderY).pack()
-        Scale(self.root, from_ = 10, to = 40, orient='horizontal', length= 200, command=self.changeSliderSegments).pack()
-        self.scaleOpacity = Scale(self.root, from_ = 100, to = 20, orient='horizontal', length= 200, command=self.changeSliderOpacity)
+
+        self.scale_x.pack()
+        self.scale_y.pack()
+        self.scale_segmets.pack()
         self.scaleOpacity.pack()
         self.scaleOpacity.set(100)
+
         Button(text="Save configuration", width=28, command=self.buttonSaveConfig).pack()
         Button(text="Load configuration", width=28, command=self.buttonLoadConfig).pack()
 
@@ -61,12 +68,12 @@ class MainWindow:
     def buttonSaveConfig(self):
         data = {
             "orders_grid" :{
-                    "width" : self.gridWindow.geometry[0],
-                    "height" : self.gridWindow.geometry[1],
-                    "pos_x" : self.gridWindow.geometry[2],
-                    "pos_y" : self.gridWindow.geometry[3],
-                    "alpha" : self.gridWindow.alpha,
-                    "segments_len" : self.gridWindow.segments_len 
+                    "width" : int(self.gridWindow.geometry[0]),
+                    "height" : int(self.gridWindow.geometry[1]),
+                    "pos_x" : int(self.gridWindow.geometry[2]),
+                    "pos_y" : int(self.gridWindow.geometry[3]),
+                    "alpha" : int(self.gridWindow.alpha),
+                    "segments_len" : int(self.gridWindow.segments_len)
                     }
                 }
         with open("config.json", "w") as file:
@@ -75,7 +82,25 @@ class MainWindow:
 
     def buttonLoadConfig(self):
         with open("config.json", "r") as file:
-            pass
+            data = json.load(file)
+            self.gridWindow.geometry[0] = data["orders_grid"]["width"]
+            self.gridWindow.geometry[1] = data["orders_grid"]["height"]
+            self.gridWindow.geometry[2] = data["orders_grid"]["pos_x"]
+            self.gridWindow.geometry[3] = data["orders_grid"]["pos_y"]
+            self.gridWindow.UpdateGeometry()
+
+            self.gridWindow.alpha = int(data["orders_grid"]["alpha"])
+            self.gridWindow.root.wm_attributes('-alpha', (int(data["orders_grid"]["alpha"]) / 100))
+
+            self.gridWindow.segments_len = int(data["orders_grid"]["segments_len"])
+            self.gridWindow.GenerateGrid()
+
+            self.scale_w.set(int(data["orders_grid"]["width"]))
+            self.scale_h.set(int(data["orders_grid"]["height"]))
+            self.scale_x.set(int(data["orders_grid"]["pos_x"]))
+            self.scale_y.set(int(data["orders_grid"]["pos_y"]))
+            self.scale_segmets.set(int(data["orders_grid"]["segments_len"]))
+            self.scaleOpacity.set(int(data["orders_grid"]["alpha"]))
 
 
     def run(self):
